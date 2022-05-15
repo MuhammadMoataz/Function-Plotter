2c37ea9b076cec89aeb8571426c2f63db87b62f0
MuhammadMoataz <mohamedmoatazali60@gmail.com>
2022-05-15 13:11:04 +0200

    Used sympy

--- plotter.py
+++ plotter.py
@@ -1,31 +1,11 @@
-# importing the required module
-import matplotlib.pyplot as plt
-import numpy as np
+from sympy import *
 
-# x axis values
-x_min = np.double(input("x min: "))
-x_max = np.double(input("x max: "))
-x = np.arange(x_min, x_max, 0.01)
-# corresponding y axis values
-# y = input("Enter the equation: ")
-# y : np.ndarray
-y = 5*x**2 + 3*x
-# y = np.ndarray(input("Enter the equation: "))
+def plotf(a,b,f):
+    plot(f, xlim = (a,b))
 
-# plotting the points
-plt.plot(x, y)
+fn = input("enter: ")
 
-
-
-# naming the x axis
-plt.xlabel('x - axis')
-# naming the y axis
-plt.ylabel('y - axis')
-
-# giving a title to my graph
-plt.title('Function Plotter!')
-
-#show legend
-# plt.legend()
-# function to show the plot
-plt.show()
+try:
+    plotf(-10, 10, fn) # or plotf(0, 2, "x**2"), or (almost) whatever
+except:
+    print("Invalid input")
